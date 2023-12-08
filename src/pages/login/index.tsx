4552ed6c6b50cb41548e706c3ec94b556ed689b3
rvahilario <rvahilario@gmail.com>
2023-12-08 20:52:28 -0300

    add: icons to login buttons

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,10 @@
 import { AuthButtons, Container, Hero, LogoContainer } from './styles'
 
 import { Button } from '@/src/components/Button'
+import Image from 'next/image'
+import githubLogo from '@/public/icons/github.svg'
+import googleLogo from '@/public/icons/google.svg'
+import rocketLogo from '@/public/icons/rocket.svg'
 
 export default function Login() {
   return (
@@ -12,9 +16,18 @@ export default function Login() {
         <p>Log in or access as a guest.</p>
 
         <AuthButtons>
-          <Button>Sign in with Google</Button>
-          <Button>Sign in with GitHub</Button>
-          <Button>Access as a guest</Button>
+          <Button onClick={() => handleSignIn('google')}>
+            <Image src={googleLogo} alt="google logo" />
+            Sign in with Google
+          </Button>
+          <Button onClick={() => handleSignIn('github')}>
+            <Image src={githubLogo} alt="github logo" />
+            Sign in with GitHub
+          </Button>
+          <Button onClick={() => handleSignIn()}>
+            <Image src={rocketLogo} alt="rocket icon" />
+            Access as a guest
+          </Button>
         </AuthButtons>
       </Hero>
     </Container>
