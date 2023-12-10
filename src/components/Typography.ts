59abc7ef2e98ec2f5f6b2c8eba52e4f2c49f5bfd
rvahilario <rvahilario@gmail.com>
2023-12-10 02:22:12 -0300

    add: Heading comp to typography

--- src/components/Typography.ts
+++ src/components/Typography.ts
@@ -11,7 +11,7 @@ export const Text = styled('p', {
       md: {
         fontSize: '$md',
       },
-      lg: {
+      xl: {
         fontSize: '$xl',
       },
     },
@@ -34,6 +34,48 @@ export const Text = styled('p', {
 
   defaultVariants: {
     size: 'md',
+    color: 'gray-100',
+  },
+})
+
+export const Heading = styled('h2', {
+  fontWeight: '$bold',
+  lineHeight: '$short',
+
+  variants: {
+    size: {
+      md: {
+        fontSize: '$md',
+      },
+      lg: {
+        fontSize: '$lg',
+      },
+      xl: {
+        fontSize: '$xl',
+      },
+      '2xl': {
+        fontSize: '$2xl',
+      },
+    },
+
+    color: {
+      'gray-100': {
+        color: '$gray100',
+      },
+      'gray-200': {
+        color: '$gray200',
+      },
+      'gray-300': {
+        color: '$gray300',
+      },
+      'gray-400': {
+        color: '$gray400',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'lg',
     color: 'gray-100',
   },
 })
