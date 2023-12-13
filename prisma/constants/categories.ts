6824551e588d7a88279f1ec687f76cf901dc3e54
rvahilario <rvahilario@gmail.com>
2023-12-13 02:43:59 -0300

    Update category names

--- prisma/constants/categories.ts
+++ prisma/constants/categories.ts
@@ -1,18 +1,18 @@
 export const categories = [
   {
-    name: 'Programação',
+    name: 'Programming',
     id: 'c9f22067-4978-4a24-84a1-7d37f343dfc2',
   },
   {
-    name: 'Educação',
+    name: 'Education',
     id: 'f1a50507-0aa7-4245-8a5c-0d0de14e9d6d',
   },
   {
-    name: 'Ficção',
+    name: 'SciFi',
     id: '8c4a4a4d-cbc4-4d2c-bb46-e95b0a536e09',
   },
   {
-    name: 'Aventura',
+    name: 'Adventure',
     id: 'e9c6d3f6-f3ec-4c52-ae28-6adcbab6ee67',
   },
   {
@@ -20,11 +20,11 @@ export const categories = [
     id: '2e65c193-325a-40c3-98f3-6c13e9b75b02',
   },
   {
-    name: 'Alegoria',
+    name: 'Allegory',
     id: 'a1d0ee25-9c9a-49c8-84eb-7af1e0dd356d',
   },
   {
-    name: 'Fábula',
+    name: 'Fable',
     id: '997f8a10-21fb-4c80-bd16-17e8b79a31a3',
   },
   {
@@ -32,19 +32,19 @@ export const categories = [
     id: '70efc33d-7d6b-4db4-bab6-524c4c4b2e2c',
   },
   {
-    name: 'Suspense',
+    name: 'Thriller',
     id: '7c8dc74a-2e03-4d72-96de-822e332e5530',
   },
   {
-    name: 'Autoajuda',
+    name: 'Self-help',
     id: 'a4d63d4e-f8ad-4a60-b7b9-9d925a2a8a92',
   },
   {
-    name: 'Arquitetura',
+    name: 'Architecture',
     id: 'a2891eaa-6d9e-48d8-a86a-10aa017d3d3f',
   },
   {
-    name: 'Terror',
+    name: 'Horror',
     id: 'a0a61b53-37d7-48ec-9b92-6db074f6d9c9',
   },
 ]
