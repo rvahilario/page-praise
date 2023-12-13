4dfcd683309f61931c848febd5924153ef89da74
rvahilario <rvahilario@gmail.com>
2023-12-13 20:05:42 -0300

    update: next.config.js with remotePatterns for image domains

--- next.config.js
+++ next.config.js
@@ -3,12 +3,27 @@ const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'api.ts', 'api.tsx'],
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-      'github.com',
-      'lh3.googleusercontent.com',
-      'images.unsplash.com',
-      's3-alpha-sig.figma.com',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 's3-alpha-sig.figma.com',
+      },
     ],
   },
 }
