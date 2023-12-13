fde00e761114198b5d47d910b860ec01fa2b87be
rvahilario <rvahilario@gmail.com>
2023-12-12 22:53:52 -0300

    add: search functionality and display ratings book in profile page

--- src/pages/profile/[id].page.tsx
+++ src/pages/profile/[id].page.tsx
@@ -1,22 +1,66 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { PageTitle } from '@/src/components/PageTitle'
-import { User } from '@phosphor-icons/react'
+import { MagnifyingGlass, User } from '@phosphor-icons/react'
 import { useRouter } from 'next/router'
 import { useSession } from 'next-auth/react'
 import { useProfileData } from '@/src/hooks/useProfileData'
+import { LinkNavigation } from '@/src/components/LinkNavigation'
+import { Container, RatingsList } from './styles'
+import { BookCardColumn } from '@/src/components/BookCardColumn'
+import { SearchInput } from '@/src/components/SearchInput'
+import { useState } from 'react'
+import { Text } from '@/src/components/Typography'
 
 const Profile = () => {
   const router = useRouter()
   const userId = router.query.id as string
   const { data: session } = useSession()
   const { data: profile } = useProfileData(userId)
+  const [search, setSearch] = useState('')
+
+  const ratings = profile?.ratings || []
   const isOwnProfile = session?.user?.id === userId
-  console.log('profile', profile)
-  console.log('isOwnProfile', isOwnProfile)
+
+  const filteredRatings = ratings?.filter((rating) => {
+    return rating.book.name.toLowerCase().includes(search.toLowerCase())
+  })
 
   return (
-    <div>
-      <PageTitle title="Profile" icon={<User size={32} />} />
-    </div>
+    <Container>
+      {isOwnProfile ? (
+        <PageTitle title="Perfil" icon={<User size={25} />} />
+      ) : (
+        <LinkNavigation
+          href="/"
+          text="Back"
+          iconSide="left"
+          color="white"
+          css={{ alignSelf: 'flex-start' }}
+        />
+      )}
+      <SearchInput
+        placeholder="Search rated book"
+        icon={<MagnifyingGlass size={20} />}
+        css={{ marginTop: 40, marginBottom: 32 }}
+        value={search}
+        onChange={({ target }) => setSearch(target.value)}
+      />
+
+      <RatingsList>
+        {filteredRatings?.map((rating) => {
+          const book = {
+            ...rating.book,
+            avgRating: rating.rate,
+          }
+          return <BookCardColumn key={rating.id} book={book} />
+        })}
+        {filteredRatings && filteredRatings.length <= 0 && (
+          <Text color="gray-400" css={{ textAlign: 'center' }}>
+            {search ? 'No results found' : 'No reviews found'}
+          </Text>
+        )}
+      </RatingsList>
+    </Container>
   )
 }
 
