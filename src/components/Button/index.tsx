72ff48361fc9813989362cfc4388926de0e0038a
rvahilario <rvahilario@gmail.com>
2023-12-14 03:39:09 -0300

    add: IconButton component to Button module

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -44,3 +44,29 @@ export const Button = styled('button', {
 export type ButtonProps = ComponentProps<typeof Button> & {
   as?: ElementType
 }
+
+export const IconButton = styled('button', {
+  width: 40,
+  height: 40,
+  borderRadius: 4,
+  background: '$gray600',
+  transition: '0.2s',
+  border: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  svg: {
+    width: 24,
+    height: 24,
+  },
+
+  '&:not(:disabled):hover': {
+    background: '$gray500',
+  },
+
+  '&:disabled': {
+    opacity: 0.5,
+    cursor: 'not-allowed',
+  },
+})
